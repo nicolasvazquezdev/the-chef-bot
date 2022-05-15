79aa4fd9b99732b85280479b61df7765525b3f6f
Nicolás Vázquez <nvazquez@novared.net>
2022-05-14 21:00:37 -0300

    to-do: finish scheduled recipes

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+from time import sleep
 import telebot
 from getRecipe import getRecipe
 from variables import token
+from datetime import datetime
 
 bot = telebot.TeleBot(token)
+now = datetime.now()
+if(now.minute <= 9):
+    minutes = f"0{now.minute}"
+else:
+    minutes = now.minute
+if(now.hour <= 9):
+    hour = f"0{now.hour}"
+else:
+    hour = now.hour
 
-@bot.message_handler(commands=['start', 'help'])
+schedule = f"{hour}:{minutes}"
+
+# /help
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, """
+Hey!
+If you want a recipe, send the command /getRecipe
+You can also search a recipe with a parameter, example:
+/getRecipe ice cream
+/getRecipe vegan
+/getRecipe dinner
+    """)
 
+# /getRecipe
 @bot.message_handler(commands=['getRecipe'])
-def send_randomRecipe(message):
+def send_recipe(message):
     chatId = message.from_user.id
     message = message.text.split()[1::] # Get the words next to the command. Those will be the tags to search the recipe
     tags = " ".join(message)
@@ -17,6 +39,27 @@ def send_randomRecipe(message):
         bot.send_message(chatId, getRecipe(tags))
     else:
         bot.send_message(chatId, "I couldn't find a recipe with those characteristics. Try something else 🔁")
+
+# /setRecipe
+recipesScheduled = []
+chat_id = ""
+@bot.message_handler(commands=['setRecipe'])
+def setRecipe(message):
+    messageUser = message.text.split()[1::] # Get the words next to the command. Those will be the tags to search the recipe
+    commandMessage = " ".join(messageUser).split(" ")
+    commandTag = commandMessage[1]
+    commandSchedule = commandMessage[0]
+    newRecipe = {"schedule": commandSchedule, "tag": commandTag}
+    recipesScheduled.append(newRecipe)
+    
+# Send the recipes that were scheduled
+def sendScheduledRecipes():
+    while True == True:
+        print(recipesScheduled)
+        print("----")
+        sleep(1)
+sendScheduledRecipes()
+
 
 @bot.message_handler()
 def echo_all(message):
@@ -30,6 +73,6 @@ def echo_all(message):
     elif("thanks" in userMsg):
         bot.send_message(chatId, "You're welcome 🙏")
     else:
-        bot.send_message(chatId, "Do you want a recipe? Send the command /getRecipe 🤖")
+        bot.send_message(chatId, "I don't understand. If you need help, send the comand /help 🤖")
 
 bot.infinity_polling()
